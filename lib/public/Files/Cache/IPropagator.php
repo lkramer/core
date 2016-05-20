e98cacf5fe47e25183617a19defa97ecd961499a
Robin Appelman <icewind@owncloud.com>
2016-05-20 15:10:18 +0200

    add propagator batching

--- lib/public/Files/Cache/IPropagator.php
+++ lib/public/Files/Cache/IPropagator.php
@@ -28,6 +28,21 @@ namespace OCP\Files\Cache;
  */
 interface IPropagator {
 	/**
+	 * Mark the beginning of a propagation batch
+	 *
+	 * Note that not all cache setups support propagation in which case this will be a noop
+	 *
+	 * Batching for cache setups that do support it has to be explicit since the cache state is not fully consistent
+	 * before the batch is committed.
+	 */
+	public function beginBatch();
+
+	/**
+	 * Commit the active propagation batch
+	 */
+	public function commitBatch();
+
+	/**
 	 * @param string $internalPath
 	 * @param int $time
 	 * @since 9.0.0
